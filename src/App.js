a9c4e18779b950fe62c34a2d9d7af2e60f011a44
Jeff Alexander <12126499+alexander-jeff@users.noreply.github.com>
2023-01-22 14:23:36 -0800

    Issue-1 Use static api for fetching quotes
    - resolves #1

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
 
 const App = () => {
   const [quote, setQuote] = useState('')
-  const [author, setAuthor] = useState('')    
-  
+  const [author, setAuthor] = useState('')
+
   const getQuote = async () => {
-    const response = await fetch('https://api.quotable.io/random')
-    const data = await response.json()
-    const { content, author } = data
-    setQuote(content)
+    const response = await fetch(
+      'https://gist.githubusercontent.com/alexander-jeff/75c996a2e1305b9c8fbeac5f84c76706/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+    )
+    const responseData = await response.json()
+    const { quotes } = responseData
+
+    const randomIndex = Math.floor(Math.random() * quotes.length)
+    const data = quotes[randomIndex]
+
+    const { quote, author } = data
+    setQuote(quote)
     setAuthor(author)
   }
 
@@ -22,23 +29,35 @@ const App = () => {
     return url
   }
 
-
   useEffect(() => {
     getQuote()
   }, [])
 
   return (
-    <div id="quote-box">
-      <div id="text">
-        <FontAwesomeIcon className="left-quote" icon={faQuoteLeft} />
-        <div className="quoted">{quote || `loading...`}</div>
+    <div id='quote-box'>
+      <div id='text'>
+        <FontAwesomeIcon
+          className='left-quote'
+          icon={faQuoteLeft}
+        />
+        <div className='quoted'>{quote || `loading...`}</div>
         {/* /<FontAwesomeIcon className="right-quote" icon={faQuoteRight} /> */}
       </div>
-      <div id="author">{author || `loading...`}</div>
-      <a id="tweet-quote" href={getTweetUrl()} target="_blank" rel="noopener noreferrer">
+      <div id='author'>{author || `loading...`}</div>
+      <a
+        id='tweet-quote'
+        href={getTweetUrl()}
+        target='_blank'
+        rel='noopener noreferrer'
+      >
         Tweet this quote
       </a>
-      <button id="new-quote" onClick={getQuote}>New Quote</button>
+      <button
+        id='new-quote'
+        onClick={getQuote}
+      >
+        New Quote
+      </button>
     </div>
   )
 }
